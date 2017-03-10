b0c3abb18d6eee63bfafb4e90337a845900f893f
David Nerjes <david.nerjes@mailbox.org>
2017-03-10 19:39:18 +0100

    gpg - decrypt and encrypt keepass2 files

---
+++ src/streams/gpgstream.h
@@ -0,0 +1,33 @@
+#ifndef GPGSTREAM_H
+#define GPGSTREAM_H
+
+#include "streams/LayeredStream.h"
+#include "gpgme++/context.h"
+#include <QIODevice>
+
+class GpgStream : public LayeredStream
+{
+    Q_OBJECT
+
+public:
+    explicit GpgStream(QIODevice* baseDevice);
+    //GpgStream(QIODevice* baseDevice, qint32 blockSize);
+    ~GpgStream();
+    bool open(QIODevice::OpenMode mode) override;
+    bool reset() override;
+    void close() override;
+
+protected:
+    qint64 readData(char* data, qint64 maxSize) override;
+    qint64 writeData(const char* data, qint64 maxSize) override;
+
+private:
+    /// \internal d-pointer class.
+    class Private;
+    /// \internal d-pointer instance.
+    Private* const d;
+    void init();
+    void keyList(QStringList& list, bool secretKeys, const QString& pattern);
+};
+
+#endif // GPGSTREAM_H
